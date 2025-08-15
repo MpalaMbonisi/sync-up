7709c696b6f1911d47faa00ec3f8fbee512e329b
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-15 15:44:38 +0200

    fix(model): Resolve @Data annotation bug

--- src/main/java/com/github/mpalambonisi/syncup/model/TaskItem.java
+++ src/main/java/com/github/mpalambonisi/syncup/model/TaskItem.java
@@ -1,19 +1,23 @@
 package com.github.mpalambonisi.syncup.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class TaskItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NonNull
     private String description;
+    @NonNull
     private boolean isCompleted;
+    @NonNull
     @ManyToOne
     private TaskList taskList;
 }
--- src/main/java/com/github/mpalambonisi/syncup/model/TaskList.java
+++ src/main/java/com/github/mpalambonisi/syncup/model/TaskList.java
@@ -1,25 +1,29 @@
 package com.github.mpalambonisi.syncup.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import java.util.List;
 import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class TaskList {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NonNull
     private String title;
+    @NonNull
     @ManyToOne
     private User owner;
+    @NonNull
     @OneToMany
     private List<TaskItem> tasks;
+    @NonNull
     @ManyToMany
     private Set<User> collaborators;
 }
