2af37202db243fdb11bb9c880c4038d45d315ea2
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-15 08:32:12 +0200

    chore: Initialise spring-boot application

---
+++ src/main/java/com/github/mpalambonisi/syncup/SyncupApplication.java
@@ -0,0 +1,13 @@
+package com.github.mpalambonisi.syncup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncupApplication.class, args);
+	}
+
+}
