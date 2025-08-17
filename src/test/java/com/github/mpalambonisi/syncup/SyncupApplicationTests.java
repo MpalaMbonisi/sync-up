f4080fd138cfd51a7a7982501acacd57bc62a0d0
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-17 07:31:43 +0200

    feat(test): Add Testcontainers to initial integration test

--- src/test/java/com/github/mpalambonisi/syncup/SyncupApplicationTests.java
+++ src/test/java/com/github/mpalambonisi/syncup/SyncupApplicationTests.java
@@ -2,9 +2,28 @@ package com.github.mpalambonisi.syncup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SyncupApplicationTests {
+
+	// This will create and start PostgreSQL container for tests
+	@Container
+	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+	// This method will override the application.properties datasource URL with dynamic one from the container
+	@DynamicPropertySource
+	static void postgresqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+	}
+
 
 	@Test
 	void contextLoads() {
