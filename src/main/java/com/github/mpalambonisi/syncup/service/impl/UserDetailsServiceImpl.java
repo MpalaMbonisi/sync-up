58543784a13c333db083a5c83b78ccb14ce5b83a
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-18 11:24:06 +0200

    feat(service): Implement UserDetailsService

--- src/main/java/com/github/mpalambonisi/syncup/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/github/mpalambonisi/syncup/service/impl/UserDetailsServiceImpl.java
@@ -1,13 +1,27 @@
 package com.github.mpalambonisi.syncup.service.impl;
 
+import com.github.mpalambonisi.syncup.model.User;
+import com.github.mpalambonisi.syncup.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // check if the user exists in the database and return user
-        return null;
+        Optional<User> user = repository.findByUsername(username);
+        if(user.isEmpty()){
+            throw new UsernameNotFoundException("Username does not exist.");
+        }
+        return user.get();
     }
 }
