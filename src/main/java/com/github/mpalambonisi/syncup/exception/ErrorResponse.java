344521e3e35490af214bd1b9482d96e0f8261100
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-19 09:54:22 +0200

    feat(exception): Add GlobalExceptionHandler and ErrorResponse

--- src/main/java/com/github/mpalambonisi/syncup/exception/ErrorResponse.java
+++ src/main/java/com/github/mpalambonisi/syncup/exception/ErrorResponse.java
@@ -1,2 +1,19 @@
-package com.github.mpalambonisi.syncup.exception;public class ErrorResponse {
+package com.github.mpalambonisi.syncup.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class ErrorResponse {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private List<String> message;
+
+    public ErrorResponse(List<String> message){
+        this.timestamp = LocalDateTime.now();
+        this.message = message;
+    }
 }
--- src/main/java/com/github/mpalambonisi/syncup/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/mpalambonisi/syncup/exception/GlobalExceptionHandler.java
@@ -1,2 +1,55 @@
-package com.github.mpalambonisi.syncup.exception;public class GlobalExceptionHandler {
+package com.github.mpalambonisi.syncup.exception;
+
+import lombok.NonNull;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.method.MethodValidationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({UsernameNotFoundException.class, UsernameExistsException.class})
+    public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException e){
+        ErrorResponse error = new ErrorResponse(Collections.singletonList(e.getMessage()));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Object> handleDataAccessException(EmptyResultDataAccessException e){
+        ErrorResponse error = new ErrorResponse(List.of("Cannot delete non-existing resource."));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException e){
+        ErrorResponse error = new ErrorResponse(List.of("Data Integrity Violation: we cannot process your request."));
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    // Handling Validation Errors (@Valid annotation)
+    @Override
+    @NonNull
+    protected ResponseEntity<Object> handleMethodValidationException(MethodValidationException ex,
+                                                                     @NonNull HttpHeaders headers,
+                                                                     @NonNull HttpStatus status,
+                                                                     @NonNull WebRequest request) {
+        List<String> errors = new ArrayList<>();
+
+        ex.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
+
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
+    }
 }
