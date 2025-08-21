f669a01272f1d099bc6a292d165426b34c831853
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-21 21:33:51 +0200

    fix(validation): Resolve email validation

--- src/main/java/com/github/mpalambonisi/syncup/dto/UserRegistrationDTO.java
+++ src/main/java/com/github/mpalambonisi/syncup/dto/UserRegistrationDTO.java
@@ -1,9 +1,6 @@
 package com.github.mpalambonisi.syncup.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,7 +26,8 @@ public class UserRegistrationDTO {
 
     @NotEmpty(message = "Email cannot be empty.")
     @NotBlank(message = "Email cannot be blank.")
-    @Email(message = "Please provide a valid email address.")
+    @Email(message = "Please provide a valid email address.", regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
 
     @NotEmpty(message = "Password cannot be empty.")
