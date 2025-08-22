b04d03f3e3b2a8a3a6ecdce64c80a733ddab6e20
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-22 22:15:31 +0200

    feat(security): Add custom authentication entry point

---
+++ src/main/java/com/github/mpalambonisi/syncup/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.github.mpalambonisi.syncup.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mpalambonisi.syncup.exception.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        ErrorResponse errorResponse = new ErrorResponse(List.of("Authentication Failed! Invalid credentials!"));
+        // Serialise the object to a JSON string and write it to the response body using the PrintWriter
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
