a377178b4c83b10dd4a3c3bca7dbba9f7b3beb93
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-23 01:26:15 +0200

    test(service): Verify saving valid tasklist

--- src/test/java/com/github/mpalambonisi/syncup/service/TaskListServiceTest.java
+++ src/test/java/com/github/mpalambonisi/syncup/service/TaskListServiceTest.java
@@ -1,2 +1,54 @@
-package com.github.mpalambonisi.syncup.service;public class TaskListServiceTest {
+package com.github.mpalambonisi.syncup.service;
+
+import com.github.mpalambonisi.syncup.dto.TaskListCreateDTO;
+import com.github.mpalambonisi.syncup.model.TaskList;
+import com.github.mpalambonisi.syncup.model.User;
+import com.github.mpalambonisi.syncup.repository.TaskListRepository;
+import com.github.mpalambonisi.syncup.service.impl.TaskListServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskListServiceTest {
+
+    @Mock
+    private TaskListRepository taskListRepo;
+    @InjectMocks
+    private TaskListServiceImpl taskListService;
+    @Mock
+    private PasswordEncoder encoder;
+
+    @Test
+    void saveTaskList_withValidTitle_shouldCreateAndReturnList(){
+        // Arrange
+        User ownerUser = new User("mbonisimpala", "Mbonisi", "Mpala",
+                "mbonisim12@gmail.com", encoder.encode("StrongPassword1234"));
+
+        TaskListCreateDTO dto = TaskListCreateDTO.builder()
+                .title("Grocery Shopping List")
+                .build();
+
+        // the save method should return the TaskList object it was passed
+        when(taskListRepo.save(any(TaskList.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // Act
+        TaskList taskListResult = taskListService.saveTaskList(ownerUser, dto);
+
+        // Assert
+        assertThat(taskListResult).isNotNull();
+        assertThat(taskListResult.getTitle()).isEqualTo(dto.getTitle());
+        assertThat(taskListResult.getOwner()).isEqualTo(ownerUser);
+
+        // Verify
+        verify(taskListRepo, times(1)).save(any(TaskList.class));
+    }
+
 }
