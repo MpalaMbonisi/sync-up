ac827573ab60f01da774c777241b150747ce4ff5
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-23 01:22:28 +0200

    feat(repository): Add findByTitle method

--- src/main/java/com/github/mpalambonisi/syncup/repository/TaskListRepository.java
+++ src/main/java/com/github/mpalambonisi/syncup/repository/TaskListRepository.java
@@ -3,5 +3,8 @@ package com.github.mpalambonisi.syncup.repository;
 import com.github.mpalambonisi.syncup.model.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
+    Optional<TaskList> findByTitle(String title);
 }
