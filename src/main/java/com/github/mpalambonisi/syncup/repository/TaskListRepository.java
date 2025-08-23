1a7ab51d0f1cccbd9bcfa5893872993d3ee1d3ac
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-23 02:49:54 +0200

    feat(repository): Add findAllByOwner method

--- src/main/java/com/github/mpalambonisi/syncup/repository/TaskListRepository.java
+++ src/main/java/com/github/mpalambonisi/syncup/repository/TaskListRepository.java
@@ -1,10 +1,13 @@
 package com.github.mpalambonisi.syncup.repository;
 
 import com.github.mpalambonisi.syncup.model.TaskList;
+import com.github.mpalambonisi.syncup.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
     Optional<TaskList> findByTitle(String title);
+    List<TaskList> findAllByOwner(User owner);
 }
