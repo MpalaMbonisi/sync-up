8e0ed8022c16f700f775a279cecaafcab2d07b86
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-23 02:25:12 +0200

    feat(controller): Implement create tasklist endpoint.

--- src/main/java/com/github/mpalambonisi/syncup/controller/TaskListController.java
+++ src/main/java/com/github/mpalambonisi/syncup/controller/TaskListController.java
@@ -3,16 +3,24 @@ package com.github.mpalambonisi.syncup.controller;
 import com.github.mpalambonisi.syncup.dto.request.AddCollaboratorsRequestDTO;
 import com.github.mpalambonisi.syncup.dto.request.RemoveCollaboratorRequestDTO;
 import com.github.mpalambonisi.syncup.dto.TaskListCreateDTO;
+import com.github.mpalambonisi.syncup.dto.response.TaskListResponseDTO;
+import com.github.mpalambonisi.syncup.model.TaskList;
+import com.github.mpalambonisi.syncup.model.User;
+import com.github.mpalambonisi.syncup.service.impl.TaskListServiceImpl;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/list")
+@RequiredArgsConstructor
 public class TaskListController {
+
+    private final TaskListServiceImpl taskListService;
 
     @GetMapping("/all")
     public ResponseEntity<HttpStatus> getAllLists(@AuthenticationPrincipal User currentUser){
@@ -20,9 +28,15 @@ public class TaskListController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<HttpStatus> createList(@Valid @RequestBody TaskListCreateDTO dto,
-                                                 @AuthenticationPrincipal User currentUser){
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<TaskListResponseDTO> createList(@Valid @RequestBody TaskListCreateDTO dto,
+                                                          @AuthenticationPrincipal User currentUser){
+        TaskList savedTaskList = taskListService.saveTaskList(currentUser, dto);
+        TaskListResponseDTO responseDTO = TaskListResponseDTO.builder()
+                .title(savedTaskList.getTitle())
+                .owner(savedTaskList.getOwner().getUsername())
+                .build();
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
