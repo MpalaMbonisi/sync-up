389549f12aed413b0f8dc0277409b1b33f9c9d21
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-28 21:45:38 +0200

    test(service): Setup initial unit test for task item service

--- src/test/java/com/github/mpalambonisi/syncup/service/TaskItemServiceTest.java
+++ src/test/java/com/github/mpalambonisi/syncup/service/TaskItemServiceTest.java
@@ -1,2 +1,30 @@
-package com.github.mpalambonisi.syncup.service;public class TaskItemServiceTest {
+package com.github.mpalambonisi.syncup.service;
+
+
+import com.github.mpalambonisi.syncup.model.User;
+import com.github.mpalambonisi.syncup.repository.TaskItemRepository;
+import com.github.mpalambonisi.syncup.repository.UserRepository;
+import com.github.mpalambonisi.syncup.service.impl.TaskItemServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskItemServiceTest {
+    @Mock
+    private TaskItemRepository taskItemRepository;
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private TaskItemServiceImpl taskItemService;
+    private User ownerUser;
+
+    @BeforeEach
+    void setUp() {
+        ownerUser = new User(1L, "mbonisimpala", "Mbonisi", "Mpala",
+                "mbonisim12@gmail.com", "StrongPassword1234");
+    }
+
 }
