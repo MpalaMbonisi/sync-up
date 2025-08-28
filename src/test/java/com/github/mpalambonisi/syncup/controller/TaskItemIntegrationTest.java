6f5206096a9f89210c6ed3708d57422dee19b6d7
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-28 21:44:18 +0200

    fix(test): Fix end of file

---
+++ src/test/java/com/github/mpalambonisi/syncup/controller/TaskItemIntegrationTest.java
@@ -0,0 +1,58 @@
+package com.github.mpalambonisi.syncup.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mpalambonisi.syncup.model.User;
+import com.github.mpalambonisi.syncup.repository.TaskItemRepository;
+import com.github.mpalambonisi.syncup.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TaskItemIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private TaskItemRepository taskItemRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder encoder;
+    private final User ownerUser = new User();
+
+    // Configure Test containers
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @BeforeEach
+    void setUp(){
+        ownerUser.setUsername("mbonisimpala");
+        ownerUser.setFirstName("Mbonisi");
+        ownerUser.setLastName("Mpala");
+        ownerUser.setEmail("mbonisim12@gmail.com");
+        ownerUser.setPassword(encoder.encode("StrongPassword1234"));
+        userRepository.save(ownerUser);
+    }
+}
