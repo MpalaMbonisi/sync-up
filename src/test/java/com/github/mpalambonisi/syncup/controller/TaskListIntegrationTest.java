c229abd3e80ec5ba6e24e270f64fe93d5d1788fa
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-28 05:47:40 +0200

    test(integration): Verify owner can add collaborator by username

--- src/test/java/com/github/mpalambonisi/syncup/controller/TaskListIntegrationTest.java
+++ src/test/java/com/github/mpalambonisi/syncup/controller/TaskListIntegrationTest.java
@@ -2,6 +2,7 @@ package com.github.mpalambonisi.syncup.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mpalambonisi.syncup.dto.TaskListCreateDTO;
+import com.github.mpalambonisi.syncup.dto.request.AddCollaboratorsRequestDTO;
 import com.github.mpalambonisi.syncup.model.TaskList;
 import com.github.mpalambonisi.syncup.model.User;
 import com.github.mpalambonisi.syncup.repository.TaskListRepository;
@@ -26,8 +27,10 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.IsIterableContaining.hasItems;
@@ -373,6 +376,55 @@ public class TaskListIntegrationTest {
         // Post-Action Verification
         Optional<TaskList> savedList = taskListRepository.findById(taskListId);
         assertThat(savedList.isPresent()).isTrue();
+    }
+
+    @Test
+    void addCollaboratorsByUsername_asOwner_shouldReturn200() throws Exception{
+        // Arrange
+        User collaborator01 = new User();
+        collaborator01.setUsername("johnsmith");
+        collaborator01.setFirstName("John");
+        collaborator01.setLastName("Smith");
+        collaborator01.setEmail("johnsmith@yahoo.com");
+        collaborator01.setPassword(encoder.encode("VeryStrongPassword"));
+        userRepository.save(collaborator01);
+
+        User collaborator02 = new User();
+        collaborator02.setUsername("nicolencube");
+        collaborator02.setFirstName("Nicole");
+        collaborator02.setLastName("Ncube");
+        collaborator02.setEmail("nicolencube@gmail.com");
+        collaborator02.setPassword(encoder.encode("ReallyStrongPassword"));
+        userRepository.save(collaborator02);
+
+        TaskList taskList = new TaskList();
+        taskList.setTitle("Grocery Shopping List");
+        taskList.setOwner(ownerUser);
+
+        TaskList savedTaskList = taskListRepository.save(taskList);
+        long taskListId = savedTaskList.getId();
+
+        Set<String> collaboratorsList = new HashSet<>();
+        collaboratorsList.add(collaborator01.getUsername());
+        collaboratorsList.add(collaborator02.getUsername());
+        AddCollaboratorsRequestDTO dto = new AddCollaboratorsRequestDTO(collaboratorsList);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/list/" + taskListId + "/collaborator/add")
+                .with(SecurityMockMvcRequestPostProcessors.user(ownerUser))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Grocery Shopping List"))
+                .andExpect(jsonPath("$.owner").value("mbonisimpala"))
+                .andExpect(jsonPath("$.collaborators.length()").value(2))
+                .andExpect(jsonPath("$.collaborators",
+                        hasItems("nicolencube", "johnsmith")));
+
+        // Post-Action Verification
+        Optional<TaskList> retrievedTaskList = taskListRepository.findById(taskListId);
+        assertThat(retrievedTaskList.isPresent()).isTrue();
+        assertThat(retrievedTaskList.get().getCollaborators()).hasSize(2);
     }
 
 }
