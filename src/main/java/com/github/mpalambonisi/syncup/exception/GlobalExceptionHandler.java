0455a385b4b75a6e626c52dfe4ff20be2ccc82cc
Mpalambonisi <mpalambonisi63@gmail.com>
2025-08-30 11:48:34 +0200

    feat(exception): handle TaskNotFoundException in GlobalHandler

--- src/main/java/com/github/mpalambonisi/syncup/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/mpalambonisi/syncup/exception/GlobalExceptionHandler.java
@@ -22,7 +22,7 @@ import java.util.List;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({UsernameNotFoundException.class, ListNotFoundException.class})
+    @ExceptionHandler({UsernameNotFoundException.class, ListNotFoundException.class, TaskNotFoundException.class})
     public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex){
         ErrorResponse error = new ErrorResponse(Collections.singletonList(ex.getMessage()));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
