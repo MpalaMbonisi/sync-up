113897d564f5fa9c492baeff6794f1318cdda534
Mpalambonisi <mpalambonisi63@gmail.com>
2025-09-01 22:03:50 +0200

    fix(test): resolve test containers error in GitHub Actions workflow

--- src/test/java/com/github/mpalambonisi/syncup/SyncupApplicationTests.java
+++ src/test/java/com/github/mpalambonisi/syncup/SyncupApplicationTests.java
@@ -2,10 +2,25 @@ package com.github.mpalambonisi.syncup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SyncupApplicationTests {
+	@Container
+	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+	@DynamicPropertySource
+	static void postgresqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+	}
+
 	@Test
 	void contextLoads() {
 	}
