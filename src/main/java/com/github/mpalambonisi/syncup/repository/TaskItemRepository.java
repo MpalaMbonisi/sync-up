77d92d3c7f90e05a9067bd62d566bbfe88e8ac4d
Mpalambonisi <mpalambonisi63@gmail.com>
2025-09-03 10:04:32 +0200

    feat(repo): add findByDescription JPA method

--- src/main/java/com/github/mpalambonisi/syncup/repository/TaskItemRepository.java
+++ src/main/java/com/github/mpalambonisi/syncup/repository/TaskItemRepository.java
@@ -3,5 +3,8 @@ package com.github.mpalambonisi.syncup.repository;
 import com.github.mpalambonisi.syncup.model.TaskItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskItemRepository extends JpaRepository<TaskItem, Long> {
+    Optional<TaskItem> findByDescription(String description);
 }
