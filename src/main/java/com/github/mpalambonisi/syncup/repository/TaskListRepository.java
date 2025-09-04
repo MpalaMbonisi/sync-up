3c7405368c5be7df89cf3b70a74e40842b5775df
Mpalambonisi <mpalambonisi63@gmail.com>
2025-09-04 15:57:04 +0200

    feat(repo): add query to find task lists by owner or collaborator

--- src/main/java/com/github/mpalambonisi/syncup/repository/TaskListRepository.java
+++ src/main/java/com/github/mpalambonisi/syncup/repository/TaskListRepository.java
@@ -3,6 +3,8 @@ package com.github.mpalambonisi.syncup.repository;
 import com.github.mpalambonisi.syncup.model.TaskList;
 import com.github.mpalambonisi.syncup.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,7 @@ import java.util.Optional;
 public interface TaskListRepository extends JpaRepository<TaskList, Long> {
     Optional<TaskList> findByTitleAndOwner(String title, User owner);
     List<TaskList> findAllByOwner(User owner);
+
+    @Query("SELECT DISTINCT tl FROM TaskList WHERE tl.owner = :user OR :user MEMBER OF tl.collaborators")
+    List<TaskList> findAllByOwnerOrCollaborator(@Param("user") User user);
 }
