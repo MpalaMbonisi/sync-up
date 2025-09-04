f1c07b1df11d2cbdf13dc8e19780424d43410906
Mpalambonisi <mpalambonisi63@gmail.com>
2025-09-04 16:06:32 +0200

    fix(repo): change JPA method to check for duplicate description only within a specific task list

--- src/main/java/com/github/mpalambonisi/syncup/repository/TaskItemRepository.java
+++ src/main/java/com/github/mpalambonisi/syncup/repository/TaskItemRepository.java
@@ -1,10 +1,10 @@
 package com.github.mpalambonisi.syncup.repository;
 
 import com.github.mpalambonisi.syncup.model.TaskItem;
+import com.github.mpalambonisi.syncup.model.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface TaskItemRepository extends JpaRepository<TaskItem, Long> {
-    Optional<TaskItem> findByDescription(String description);
+    Optional<TaskItem> findByDescriptionAndTaskList(String description, TaskList taskList);
 }
